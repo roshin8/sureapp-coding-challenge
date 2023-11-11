e0ea69e970574d54179ee0f3d4d5f44a831f6df2
Roshin <theroshin@gmail.com>
2023-11-11 04:58:13 -0600

    Add Tests

--- insurance_app/tests.py
+++ insurance_app/tests.py
@@ -84,5 +84,55 @@ class QuotePricingTests(TestCase):
         self.assertEqual(pricing_details["Monthly Taxes"], 0.15)
         self.assertEqual(pricing_details["Monthly Total"], 30.15)
 
+
+class QuoteTests(APITestCase):
+    def test_create_quote_valid_data(self):
+        data = {
+            'buyer_name': 'John Doe',
+            'coverage_type': 'Basic',
+            'state': 'California',
+            'has_pet': True,
+            'coverage': {'flood': True}
+        }
+
+        response = self.client.post('/api/create_quote/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn('id', response.data)
+        quote = Quote.objects.get(id=response.data['id'])
+        self.assertEqual(quote.coverage_type, data['coverage_type'])
+        self.assertEqual(quote.state, data['state'])
+
+
+    def test_create_quote_invalid_data(self):
+        data = {
+            'coverage_type': 'InvalidType',
+            'state': 'InvalidState',
+            'has_pet': 'NotBoolean',
+            'coverage': {'invalid_key': 'value'}
+        }
+
+        response = self.client.post('/api/create_quote/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_quote_valid_id(self):
+        quote = Quote.objects.create(coverage_type='Basic', state='California', has_pet=True)
+        response = self.client.get(f'/api/get_quote/{quote.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_quote_invalid_id(self):
+        response = self.client.get('/api/get_quote/999/')  # Assuming 999 is an invalid ID
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_invalid_data_format(self):
+        invalid_data = 'invalid_data'
+        response = self.client.post('/api/create_quote/', invalid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_quote_with_missing_fields(self):
+        incomplete_data = {'coverage_type': 'Basic'}
+        response = self.client.post('/api/create_quote/', incomplete_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
 if __name__ == '__main__':
     unittest.main()
